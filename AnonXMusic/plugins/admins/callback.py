45033e767ebe20033b9c88c579c6f2716db9703c
Willie Deleon <116621516+WDELEON2@users.noreply.github.com>
2024-05-07 00:39:22 +0530

    Update callback.py

--- AnonXMusic/plugins/admins/callback.py
+++ AnonXMusic/plugins/admins/callback.py
@@ -3,10 +3,10 @@ import asyncio
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from SmileyMusic import YouTube, app
+from SmileyMusic.core.call import Anony
+from SmileyMusic.misc import SUDOERS, db
+from SmileyMusic.utils.database import (
     get_active_chats,
     get_lang,
     get_upvote_count,
@@ -17,11 +17,11 @@ from AnonXMusic.utils.database import (
     music_on,
     set_loop,
 )
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.formatters import seconds_to_min
-from AnonXMusic.utils.inline import close_markup, stream_markup, stream_markup_timer
-from AnonXMusic.utils.stream.autoclear import auto_clean
-from AnonXMusic.utils.thumbnails import get_thumb
+from SmileyMusic.utils.decorators.language import languageCB
+from SmileyMusic.utils.formatters import seconds_to_min
+from SmileyMusic.utils.inline import close_markup, stream_markup, stream_markup_timer
+from SmileyMusic.utils.stream.autoclear import auto_clean
+from SmileyMusic.utils.thumbnails import get_thumb
 from config import (
     BANNED_USERS,
     SOUNCLOUD_IMG_URL,
